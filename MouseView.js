ef570c460d41f766f3f1c6954a3f5b80152fda39
Alex <a.irvine.10@aberdeen.ac.uk>
2020-06-03 15:12:16 +0100

    Added data logging and local storage functions

--- MouseView.js
+++ MouseView.js
@@ -17,7 +17,7 @@
     mouseview.datalogger = mouseview.datalogger || {} // for logging data 
     mouseview.params = mouseview.params || {} // for parameters 
     mouseview.animator_raf = mouseview.animator_raf || {} // holder for request animationframe
-    
+    mouseview.timing = mouseview.timing || {}
     // her we can setup some default settings
     
     // parameters for the aperture 
@@ -29,8 +29,6 @@
     mouseview.params.overlayAlpha = 0.99 // how transparent the overlay will be
     mouseview.params.overlayGaussian = 0 // SD in pixels for the gaussian blur filter NOT WORKING, will blur everything under div
     
-    // parameters for tracking 
-    mouseview.params.sampleRate = 100 // sample rate in Hz for recording position
     
     // holders for overlay width and height 
     mouseview.params.overHeight = mouseview.params.overHeight || window.visualViewport.height
@@ -41,10 +39,21 @@
     mouseview.params.offset.X = 0
     mouseview.params.offset.Y = 0
     
-    // holders for mouseposition
-    mouseview.datalogger.x = null
-    mouseview.datalogger.y = null
+    // holders for current mouseposition
+    mouseview.datalogger.X = null
+    mouseview.datalogger.Y = null
     
+    // holders for recording state
+    mouseview.datalogger.tracking = false
+    
+    // Array to contain objects
+    mouseview.datalogger.data = []
+    
+    // parameters for timing
+    mouseview.timing.sampleRate = 16.66 // wanted frame time, set as 0 to acheive best, but inconsistent timing
+    mouseview.timing.lastTime = 0.0 // holder for last frame time
+    mouseview.timing.startTime = 0.0 // holder for start time 
+    mouseview.timing.finishTime = 0.0 // holder for finish time
     // Private functions 
     
     function init(){
@@ -69,8 +78,6 @@
         overlay.style.top = '0px'
         overlay.style.pointerEvents = 'none'
         
-
-        //TODO: deal with scrolling offset 
         
         // set mouse listener to update position on mouse move
         document.body.addEventListener('mousemove', event => {
@@ -79,7 +86,6 @@
         }, false);
         
         // add event listeners for touch-screen
-        // TODO: handle multiple touches at the same time, at the moment we just take first in list
         document.body.addEventListener('touchstart', event => {
             mouseview.datalogger.x = event.touches[0].clientX - mouseview.params.offset.X;
             mouseview.datalogger.y = event.touches[0].clientY - mouseview.params.offset.Y;
@@ -129,7 +135,7 @@
     
     // function to deal with stuff at the animation frame level
     // this will be called using rAF callback
-    function updateFrame(){
+    function updateFrame(timestamp){
         //console.log([mouseview.datalogger.x, mouseview.datalogger.y])
         var overlay = document.getElementById('overlay')
         //overlay.style.setProperty('backdrop-filter',"blur(" + mouseview.params.overlayGaussian + "px)") //blurs everything
@@ -159,7 +165,20 @@
             
         }
         
+        // if logging log mouse position
+        if (mouseview.datalogger.tracking === true){
+            // if we have reached desired sampling interval
+            console.log(timestamp, mouseview.timing.lastTime, mouseview.timing.sampleRate)
+            
+            if ( (timestamp - mouseview.timing.lastTime) >= mouseview.timing.sampleRate) {
+                logPosition(mouseview.datalogger.x, mouseview.datalogger.y, timestamp) //log position
+                mouseview.timing.lastTime = timestamp // update last timestamp
+            }
+        }
+        
+        // next frame
         mouseview.animator_raf = requestAnimationFrame(updateFrame);
+
     }
     
     // update the overlay canvas itself with new settings
@@ -182,17 +201,60 @@
     
    // Start tracking the mouse movements
     function startTracking(){
-        
+        mouseview.datalogger.tracking = true
+        mouseview.timing.startTime = window.performance.now()
+        console.log('Started recording data')
     }
     
     // Stop tracking the mouse movements
     function stopTracking(){
-        
+        mouseview.datalogger.tracking = false
+        mouseview.timing.finishTimeTime = window.performance.now()
+        console.log('Stopped recording data')
+        console.log(mouseview.datalogger.data)
+    }
+    
+    // logging data
+    function logPosition(xpos, ypos, timestamp){
+        mouseview.datalogger.data.push({
+            x: xpos,
+            y: ypos,
+            time: timestamp - mouseview.timing.startTime
+        })
+    }
+    
+    //storing data locally (helpful for multiple pages)
+    function storeData(){
+        mouseview.datalogger.data.unshift(window.location.pathname) // add pathname to beginning of data
+        localStorage.setItem("mouseview_data", JSON.stringify(mouseview.datalogger.data));
+    }
+    
+    //getting local data (helpful for plotting data)
+    
+    function getData(){
+        mouseview.datalogger.data = JSON.parse(localStorage.getItem("mouseview_data") || []);
+        mouseview.datalogger.data.push(window.location.pathname) // add pathname to end of data
     }
     
     // Public functions
     mouseview.init = () => {
         init()
+    }
+    
+    mouseview.startTracking = () => {
+        startTracking()
+    }
+    
+    mouseview.stopTracking = () => {
+        stopTracking()
+    }
+    
+    mouseview.storeData = () => {
+        storeData()
+    }
+    
+    mouseview.getData = () => {
+        getData()
     }
     // Setters 
     
--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Demo available [here](https://mouseview.netlify.app/demo.html)
 
 # Getting Started
 
+## Install and setup
 MouseView.js is designed to inject a layer over a webpage. Simply include the script on in your websites **body** tag.
  
 The most up-to-date one is hosted on netlify:
@@ -29,3 +30,35 @@ mouseview.params.overlayAlpha = 0.99
 mouseview.init()
 
 ```
+
+## Tracking 
+To start recording mouse movements you use the following functions
+```JavaScript
+mouseview.startRecording() // this starts a record and logs timestamps in console
+mouseview.stopRecording() // this stops and logs the data into the console 
+```
+The tracking data is stored in mouseviews namespace as an Array of objects with properties X, Y and time. Representing coordinates of the mouse on the viewport (not the page, relevant if scrolling is involved -- this will be updated in time), and the time in milliseconds from the recording start
+
+```JavaScript
+mouseview.datalogger.data
+```
+You can also request a target interval for data storage in milliseconds. The default is 16.66ms, which is a frame refresh on a 60Hz monitor. This is dependent on the ability to flip a frame, so sometimes this may not be exact!
+
+## Local Storage
+Mouseview.js allows you to store and retrieve data on the clients browser. This is helpful for tracking accross multiple pages, or presenting previous data on screen. When you store data it appends that page path to the beginning of the data array. When you retrieve data it will append the path of the page to the end of data array, so you can tell the difference between site pages. 
+
+```JavaScript
+// run this  before user navigates away
+mouseview.storeData() // this starts a record and logs timestamps in console
+
+// then on a new page to retrieve
+mouseview.getData()
+
+// now the namespace contains all the old data, but with the old path name at the start, and the new one at the end 
+console.log(mouseview.datalogger.data)
+
+```
+
+
+
+
