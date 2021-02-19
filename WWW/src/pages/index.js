341806c2602bc43dfcd54247c7843566211bb7ed
Alex Irvine <Alexander.Irvine@mrc-cbu.cam.ac.uk>
2021-02-19 14:49:19 +0000

    center particle background

--- WWW/src/pages/index.js
+++ WWW/src/pages/index.js
@@ -60,24 +60,10 @@ function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Web eye tracking without the eyes <head />">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
-            <Particles
-                params={{
-                   "particles":{"number":{"value":237,"density":{"enable":true,"value_area":1104.8066982851817}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":2,"direction":"none","random":true,"straight":false,"out_mode":"bounce","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"bubble"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":215.7842157842158,"size":11.988011988011989,"duration":4.155844155844156,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true
-                }} 
-                style={{
-                    position: 'absolute',
-                    pointerEvents: 'none',
-                    width: '100%', 
-                    height: '100%',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    zIndex: 1
-                }}
-              />
+           
           <img className={styles.heroImage} src={siteConfig.customFields.heroLogo} alt={siteConfig.tagline} />
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
@@ -91,7 +77,23 @@ function Home() {
             </Link>
           </div>
         </div>
-
+ <Particles
+                params={{
+                   "particles":{"number":{"value":350,"density":{"enable":true,"value_area":1000.8066982851817}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":2,"direction":"none","random":true,"straight":false,"out_mode":"bounce","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"bubble"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":100.7842157842158,"size":9.988011988011989,"duration":4.155844155844156,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true
+                }} 
+                style={{
+                    position: 'absolute',
+                    pointerEvents: 'none',
+                    width: '100%', 
+                    height: '100%',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    left: 0, 
+                    right: 0,
+                    top: 0,
+                    bottom: 0
+                }}
+              />
       </header>
       <main>
         {features && features.length > 0 && (
